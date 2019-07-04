cd7078f4db50efb405c918e268510aa311a0d6bf
sev7e0 <sev7e0@gmail.com>
2019-07-04 22:55:12 +0800

    change internal-cache project structure to spring-boot

---
+++ internal-cache/src/main/java/com/sev7e0/data/InternalCacheApplication.java
@@ -0,0 +1,13 @@
+package com.sev7e0.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalCacheApplication.class, args);
+    }
+
+}
